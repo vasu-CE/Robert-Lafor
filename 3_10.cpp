8cff49f57a776476d4d3d7b1bb93f62b075f39c8
vasu-CE <155806154+vasu-CE@users.noreply.github.com>
2024-01-08 21:59:55 +0530

    Create 3_10.cpp

---
+++ 3_10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int i,year=0;
+    float intrate,famo,amo,temp=0;
+    
+    cout << "Enter amount : ";
+    cin >> amo;
+    cout << "Enter interest rate : ";
+    cin >> intrate;
+    cout << "Enter final amount : ";
+    cin >> famo;
+
+    while(temp<=famo)
+    {
+        amo=amo+(amo*(intrate/100));
+        year++;
+        temp=amo;
+    }
+    cout << year-1;
+    return 0;
+}
